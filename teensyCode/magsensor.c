2b2f161d74b0c1a5b7eb86f6d31cb3d71f230152
Cody Campbell <projectcontrive@gmail.com>
2017-02-06 11:55:30 -0500

    started joystick code

---
+++ teensyCode/magsensor.c
@@ -0,0 +1,69 @@
+//CODY CAMPBELL
+//PROJECT CONTRIVE
+//This code is adapted from the magsensor example code provided by adafruit.
+
+//using this i2c library to access SDA1 and SCL1 from the back of the teensy
+#include <i2c_t3-master>
+
+//make sure these libraries reference WIRE1 instead of WIRE
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+
+void displaySensorDetails(void){
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void setup(void){
+  Serial.begin(9600);
+  Serial.println("Magnetometer Test"); Serial.println("");
+
+  /* Enable auto-gain */
+  mag.enableAutoRange(true);
+
+  /* Initialise the sensor */
+  if(!mag.begin()){
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("No LSM303 detected, check libraries!! (adafruit libraries must reference wire1 - NOT wire)");
+    Serial.println("entering infinite loop");
+    while(1);
+  }
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+}
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  mag.getEvent(&event);
+
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+
+  /* Note: You can also get the raw (non unified values) for */
+  /* the last data sample as follows. The .getEvent call populates */
+  /* the raw values used below. */
+  // Serial.print("X Raw: "); Serial.print(mag.raw.x); Serial.print("  ");
+  // Serial.print("Y Raw: "); Serial.print(mag.raw.y); Serial.print("  ");
+  // Serial.print("Z Raw: "); Serial.print(mag.raw.z); Serial.println("");
+
+  /* Delay before the next sample */
+  delay(500);
+}
